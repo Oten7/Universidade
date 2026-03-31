a7424419863bc3f4aa15203dfb25f36abd55ccb0
Oten7 <netinhogames3@gmail.com>
2026-03-31 11:01:35 -0300

    atvds

---
+++ pytthon aula/conceito_Iniciais.py
@@ -0,0 +1,10 @@
+# primeiro_numero = 7
+# segundo_numero = 5
+
+# print(primeiro_numero + segundo_numero)
+# print(primeiro_numero - 5)
+# print(7 * segundo_numero)
+# print(7 / segundo_numero) 
+
+nome = "Neto"
+print("Ola" + nome)
